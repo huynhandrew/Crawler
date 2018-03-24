9bcfc8df1f0c6cc9adc4dc03326617954b7c6b1a
ahuynh <nytrickacid@gmail.com>
2018-03-24 17:26:39 -0400

    finished linear exploration with end room check

--- overunderbot/app.js
+++ overunderbot/app.js
@@ -5,6 +5,8 @@ var twilio = require("twilio");
 
 var app = express();
 
+var CurrRoomKey = 'room0';
+var CurrRoomIndex = 0;
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -18,6 +20,9 @@ function Game(){
     this.Player = null;
     this.GameTurn = "Player"
     this.fCurstate = this.fWelcoming;
+
+    this.Dungeon = new Dungeon();  
+    
     
     this.fWelcoming = function(req, twiml){
         twiml.message("Welcome to the Crawler. 1 for New Game | 2 for Load Game");
@@ -38,21 +43,56 @@ function Game(){
     }
 
     this.fStartGame = function(req, twiml){
-        this.Dungeon = new Dungeon();  
-        this.CurrRoomIndex = 0;
-        this.CurrRoom = this.Dungeon.listDungeonRooms[this.CurrRoomIndex];
-        twiml.message("Hello " + this.Player + ". You've arrived in a room with a door coloured: " + this.CurrRoom.room.door + " .");
+        twiml.message("Hello " + this.Player + ". You've arrived in a room, what would you like to do? 1. Look for an exit 2. Look for monsters.");
+        this.fCurstate = this.fQuestionMaster;
     }    
+
+    this.fQuestionMaster = function(req, twiml){
+        this.CurrRoom = this.Dungeon.listDungeonRooms[CurrRoomIndex];
+        if (req.body.Body == 1 && CurrRoomIndex < 4){
+            twiml.message("You find a "+this.CurrRoom[CurrRoomKey].door + " coloured door. 1. Enter it 2. Do something else");
+            this.fCurstate = this.DoorScenario;
+        }
+        else if (req.body.Body == 1 && CurrRoomIndex == 4){
+            twiml.message("There are no doors. 2. Look for monsters.");            
+        }
+        else if (req.body.Body == 2){
+            twiml.message("You find monsters");
+        }
+        else if (req.body.Body == 3){
+            twiml.message("You find a single chest");
+        }
+        else {
+            twiml.message("Use numbers to enter an action.");
+        }
+    }
+
+    this.DoorScenario = function(req, twiml){
+        if (req.body.Body == 1){
+            twiml.message("You open the door. Send anything to continue.");
+            CurrRoomIndex++;
+            CurrRoomKey = NewRoom(CurrRoomIndex);
+            this.fCurstate = this.fStartGame;
+        }
+        else {
+            this.fCurstate = this.fStartGame;
+        }
+    }
+
     this.fCurstate = this.fWelcoming;
+}
+
+function NewRoom(index){
+    return 'room' + index;
 }
 
 /* DUNGEON CLASS */
 function Dungeon(){
     this.listDungeonRooms = [
-        {room: new DungeonRoom("blue")},
-        {room: new DungeonRoom("yellow")},
-        {room: new DungeonRoom("green")},
-        {room: new DungeonRoom("purple")}
+        {room0: new DungeonRoom("blue")},
+        {room1: new DungeonRoom("yellow")},
+        {room2: new DungeonRoom("green")},
+        {room3: new DungeonRoom("purple")}
     ];
 }
 
@@ -60,9 +100,9 @@ function Dungeon(){
 function DungeonRoom(colour){
     this.door = colour;
     this.listMonsters = [
-        {monster1: new Monster("Skeleton", 5)},
-        {monster2: new Monster("Goblin", 3)},
-        {monster3: new Monster("Annoying Bat", 2)}
+        {monster0: new Monster("Skeleton", 5)},
+        {monster1: new Monster("Goblin", 3)},
+        {monster2: new Monster("Annoying Bat", 2)}
     ];
 }
 
@@ -81,6 +121,7 @@ Monster.prototype.attack = function(){
 
 /* PLAYER CLASS */
 function Player(){
+    this.name = "";
     this.hitpoints = 20;
     this.attack = Math.ceil(Math.random()*7);
 }
